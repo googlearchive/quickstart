dcde340013be918776e1e29973ccbce4ad47cca7
Thibault Sottiaux <thso@google.com>
2016-04-20 10:07:11 +0100

    Sync

---
+++ web/main.dart
@@ -0,0 +1,9 @@
+import 'package:angular2/core.dart';
+import 'package:angular2/platform/browser.dart';
+
+@Component(selector: 'my-app', template: '<h1>My First Angular 2 App</h1>')
+class AppComponent {}
+
+main() {
+  bootstrap(AppComponent);
+}
