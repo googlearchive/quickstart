bf4bd4614f70efeede55c4dc55feae0d9ba99616
Patrice Chalin <pchalin@gmail.com>
2016-11-17 05:32:11 -0800

    Sync with 061e50f
    Synced with angular/angular.io master branch, commit 061e50f:
    https://github.com/angular/angular.io/tree/061e50fa065b9e81dbebc2ce4af94dfa98a5942e/public/docs/_examples/quickstart/dart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,4 +1,4 @@
 import 'package:angular2/core.dart';
 
-@Component(selector: 'my-app', template: '<h1>My First Angular App</h1>')
+@Component(selector: 'my-app', template: '<h1>Hello Angular!</h1>')
 class AppComponent {}
