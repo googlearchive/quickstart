9cbfc249abc63d6bc20184be5aea76826defeb1d
Patrice Chalin <pchalin@gmail.com>
2016-11-22 05:22:09 -0800

    Sync with 434308a
    Synced with angular/angular.io master branch, commit 434308a:
    https://github.com/angular/angular.io/tree/434308ab1b3eb6aeb66382e1ca615358827ac859/public/docs/_examples/quickstart/dart

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular2/platform/browser.dart';
 
-import 'package:angular2_quickstart/app_component.dart';
+import 'package:angular_quickstart/app_component.dart';
 
 void main() {
   bootstrap(AppComponent);
