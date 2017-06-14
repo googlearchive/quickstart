eeb0b907dec989e57623cf3ddb6130d5567fe34b
Patrice Chalin <pchalin@gmail.com>
2017-06-14 12:31:29 -0400

    Sync with 7537f30
    Synced with dart-lang/site-webdev master branch, commit 7537f30:
    https://github.com/dart-lang/site-webdev/tree/7537f30dd657292821e343ecd2c4c1c638ce15f6/examples/ng/doc/quickstart

--- test/app_test.dart
+++ test/app_test.dart
@@ -5,7 +5,7 @@ import 'package:angular2/angular2.dart';
 import 'package:angular_test/angular_test.dart';
 import 'package:test/test.dart';
 
-import 'package:angular_quickstart/app_component.dart';
+import 'package:angular_app/app_component.dart';
 
 @AngularEntrypoint()
 void main() {
--- web/main.dart
+++ web/main.dart
@@ -3,7 +3,7 @@
 
 import 'package:angular2/platform/browser.dart';
 
-import 'package:angular_quickstart/app_component.dart';
+import 'package:angular_app/app_component.dart';
 
 void main() {
   bootstrap(AppComponent);
