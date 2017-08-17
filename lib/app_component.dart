4d272798f1d1c8b6b123ffe7d80f47a54f13d391
Patrice Chalin <pchalin@gmail.com>
2017-08-16 21:11:22 -0400

    Sync with 6746288
    Synced with dart-lang/site-webdev 4-dev branch, commit 6746288:
    https://github.com/dart-lang/site-webdev/tree/67462888e8be9db94471ba0216ae9e85810c8ce5/examples/ng/doc/quickstart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2017. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/angular2.dart';
+import 'package:angular/angular.dart';
 
 @Component(
   selector: 'my-app',
