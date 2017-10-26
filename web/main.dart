e6c812b157e7c7583449aa33a8ecd83f86c40171
Patrice Chalin <pchalin@gmail.com>
2017-10-26 04:44:26 -0400

    Sync with 2b61f12
    Synced with dart-lang/site-webdev 5-dev branch, commit 2b61f12:
    https://github.com/dart-lang/site-webdev/tree/2b61f1275e18e7d107360ae1ad04d8218d89de77/examples/ng/doc/quickstart

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2017. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:angular/angular.dart';
 
 import 'package:angular_app/app_component.dart';
