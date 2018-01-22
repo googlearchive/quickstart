93fe25587bc8f2ede723ab588a3bd46cd989f3fb
Patrice Chalin <pchalin@gmail.com>
2018-01-22 17:17:17 -0500

    Sync with 27b0038
    Synced with dart-lang/site-webdev 5-dev branch, commit 27b0038:
    https://github.com/dart-lang/site-webdev/tree/27b003800e1ece68f4c020545e76ef43003c3148/examples/ng/doc/quickstart

--- web/main.dart
+++ web/main.dart
@@ -2,6 +2,8 @@ import 'package:angular/angular.dart';
 
 import 'package:angular_app/app_component.dart';
 
+import 'main.template.dart' as ng;
+
 void main() {
-  bootstrap(AppComponent);
+  bootstrapStatic(AppComponent, [], ng.initReflector);
 }
